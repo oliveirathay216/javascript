d14c471485dbc0e9816487239dc9739a8ec36f8a
oliveirathay216 <oliveirathay216@gmail.com>
2023-10-02 11:59:06 -0300

    projetojava

---
+++ javascript.js
@@ -0,0 +1,37 @@
+alert('Seja Bem-vindo!♥')
+
+//  var nome= prompt('Digite seu nome:');
+// alert(` Certo! Seja Bem-vindo! ${nome}!`);
+
+// var n1 = parseFloat (prompt( 'Digite o primeiro número'));
+// var n2 = parseFloat (prompt(' Digite o segundo número'));
+// alert ( 'A soma é: ' + (n1+n2)); parseFloat
+
+// var n1 = parseFloat (prompt( 'Digite a primeira nota'));
+// var n2 = parseFloat (prompt(' Digite a segunda nota'));
+// var media = (n1+n2) /2;
+
+// alert ( 'A média é: ' + media );
+
+// if(media >=7 ){
+//     alert('Aprovado♥');
+// } else{
+//     alert ('Reprovado:(');
+// }
+
+// var n = parseInt(prompt('Digite um número!'));
+
+// if (n >= 0  && n <=100) {
+//    alert('O número atende aos critérios')
+// }else{
+//     alert('O número não atende aos critérios')
+// }
+
+var n = parseInt(prompt('Digite um número inteiro:'));
+
+
+if (n >= 0 && n <=100 && n%2 === 0){
+    alert('O número atende aos critérios')
+}else{
+    alert('O número não atende aos critérios')
+}
